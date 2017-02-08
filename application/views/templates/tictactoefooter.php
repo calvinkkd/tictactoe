210df16a469d5bac5c53893a7c129917a74a4b7a
calvinkkd <calvincusi27@gmail.com>
2017-02-08 14:46:40 +0100

    removed footer and header

--- application/views/templates/tictactoefooter.php
+++
@@ -1,8 +0,0 @@
-<?php // here goes the js file ?>
-
-<script src="<?php echo base_url('assets/js/button_login'); ?>"></script>
-<script src="<?php echo base_url('assets/js/bootstrap.js'); ?>"></script>
-
-
-</body>
-</html>
--- application/views/templates/tictactoeheader.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: adminkofi
- * Date: 6/02/17
- * Time: 22:09
- */
-
-echo link_tag('assets/css/style.css');
